58eff70e784e8aebffd11979f601cc4ac69c8e1c
roscent <ofu24001@byui.edu>
2025-02-19 07:39:56 +0100

    week07

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("Hello World! This is Roscent's ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 02, 19), 30, 4.8),
+            new Cycling(new DateTime(2025, 02, 19), 45, 20.0),
+            new Swimming(new DateTime(2025, 02, 19), 25, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
